33f526791ea9612b57446a689c98ce02ee5a4b1d
bernie <bernie@tutanota.com>
2016-06-08 18:48:09 +0200

    basic test: size

--- fractalsfun/tests/all_tests.py
+++ fractalsfun/tests/all_tests.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 
+from fractalsfun.fractalcluster import FractalCluster
 import unittest
 import numpy
 
 class test_Sanity(unittest.TestCase):
     def test_nothing(self):
         pass
+
+class test_Basic(unittest.TestCase):
+    def test_size(self):
+        fc = FractalCluster(1000, 1.6)
+        self.assertTrue(fc.size >= 1000)
+
+    def test_finalsize(self):
+        fc = FractalCluster(1000, 1.6)
+        self.assertEqual(len(fc.positions), 1000)
 
